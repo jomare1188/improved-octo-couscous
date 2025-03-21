a8382f233de9e5f7f8788e7bdf905c93e78f87dd
jorge <jomare1188@gmail.com>
2025-03-21 12:04:30 -0300

    rename make_expression_matrix.r for make_stack_plots.r

--- RNAseq/code/make_stack_plots.r
+++ RNAseq/code/make_stack_plots.r
@@ -7,10 +7,9 @@ library("igraph", lib.loc="/Storage/data1/jorge.munoz/NRGSC.new/libraries")
 setwd("/Storage/data1/jorge.munoz/DOLORES/RNAseq/code")
 
 ## make histogram for DEG in all contraste (upregulated and downregulated)
-contrast_results_dir <- "/Storage/data1/jorge.munoz/DOLORES/RNAseq/results"
+contrast_results_dir <- "/Storage/data1/jorge.munoz/DOLORES/RNAseq/results/contrasts"
 log2foldthreshold <- 2
 #contrast 1
-library("dplyr", lib.loc = "/Storage/data1/jorge.munoz/NRGSC.new/libraries")
 
 #old
 #contrast_1 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_M_D_vs_8_M_D", "/", "5_M_D_vs_8_M_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
@@ -19,15 +18,15 @@ library("dplyr", lib.loc = "/Storage/data1/jorge.munoz/NRGSC.new/libraries")
 
 #contrast_3 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_M_W_vs_8_M_W", "/", "5_M_W_vs_8_M_W.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
 
-#contrast_4 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_M_D_vs_5_M_W", "/", "5_M_D_vs_5_M_W.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
+#contrast_4 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_M_W_vs_5_T_D", "/", "5_M_W_vs_5_T_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
 
-#contrast_5 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","8_M_D_vs_8_M_W", "/", "8_M_D_vs_8_M_W.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
+#contrast_5 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","8_M_W_vs_8_T_D", "/", "8_M_W_vs_8_T_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
 
 # new
 
-contrast_1 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_T_D_vs_5_M_D", "/", "5_T_D_vs_5_M_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
+contrast_1 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","5_M_W_vs_5_T_D", "/", "5_M_W_vs_5_T_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
 
-contrast_2 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","8_T_D_vs_8_M_D", "/", "8_T_D_vs_8_M_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
+contrast_2 <-as.data.frame(read.table(paste0(contrast_results_dir,"/","8_M_W_vs_8_T_D", "/", "8_M_W_vs_8_T_D.DESeq.DE_results"), header = T) %>% filter(abs(log2FoldChange) > log2foldthreshold ) %>% filter((padj) < 0.01),header = F)
 
 
 
@@ -36,31 +35,31 @@ df3 <- data.frame(matrix(ncol = 3, nrow = 4))
 colnames(df3) <- c("Contrast", "Type", "DEGs") 
 # contrastast 5_T_D_vs_5_M_D
 # up
-df3[1,1] <- "5_T_D_vs_5_M_D"
+df3[1,1] <- "5_M_W_vs_5_T_D"
 df3[1,2] <- "Up" 
 df3[1,3] <- dim(contrast_1[contrast_1$log2FoldChange > 1,])[1]
 names <- rownames(contrast_1[contrast_1$log2FoldChange > 1,])
-write.table(names, paste0(contrast_results_dir, "/","5_T_D_vs_5_M_D", "/", "5_T_D_vs_5_M_D_up"), col.names = F, row.names = F, quote = F)
+write.table(names, paste0(contrast_results_dir, "/","5_M_W_vs_5_T_D", "/", "5_M_W_vs_5_T_D_up"), col.names = F, row.names = F, quote = F)
 # down
-df3[2,1] <- "5_T_D_vs_5_M_D"
+df3[2,1] <- "5_M_W_vs_5_T_D"
 df3[2,2] <- "Down" 
 df3[2,3] <- dim(contrast_1[contrast_1$log2FoldChange < 1,])[1]
 names <- rownames(contrast_1[contrast_1$log2FoldChange < 1,])
-write.table(names, paste0(contrast_results_dir, "/","5_T_D_vs_5_M_D", "/","5_T_D_vs_5_M_D_down"), col.names = F, row.names = F, quote = F)
+write.table(names, paste0(contrast_results_dir, "/","5_M_W_vs_5_T_D", "/","5_M_W_vs_5_T_D_down"), col.names = F, row.names = F, quote = F)
 
 # contrastast 8_T_D_vs_8_M_D
 # up
-df3[3,1] <- "8_T_D_vs_8_M_D"
+df3[3,1] <- "8_M_W_vs_8_T_D"
 df3[3,2] <- "Up"
 df3[3,3] <- dim(contrast_2[contrast_2$log2FoldChange > 1,])[1]
 names <- rownames(contrast_2[contrast_2$log2FoldChange > 1,])
-write.table(names, paste0(contrast_results_dir, "/","8_T_D_vs_8_M_D", "/", "8_T_D_vs_8_M_D_up"), col.names = F, row.names = F, quote = F)
+write.table(names, paste0(contrast_results_dir, "/","8_M_W_vs_8_T_D", "/", "8_M_W_vs_8_T_D_up"), col.names = F, row.names = F, quote = F)
 # down 
-df3[4,1] <- "8_T_D_vs_8_M_D"
+df3[4,1] <- "8_M_W_vs_8_T_D"
 df3[4,2] <- "Down"
 df3[4,3] <- dim(contrast_2[contrast_2$log2FoldChange < 1,])[1]
 names <- rownames(contrast_2[contrast_2$log2FoldChange < 1,])
-write.table(names, paste0(contrast_results_dir, "/","8_T_D_vs_8_M_D", "/","8_T_D_vs_8_M_D_down"), col.names = F, row.names = F, quote = F)
+write.table(names, paste0(contrast_results_dir, "/","8_M_W_vs_8_T_D", "/","8_M_W_vs_8_T_D_down"), col.names = F, row.names = F, quote = F)
 
 # contrastast 5_M_W_vs_8_M_W
 # up
@@ -75,32 +74,32 @@ write.table(names, paste0(contrast_results_dir, "/","8_T_D_vs_8_M_D", "/","8_T_D
 #df3[6,3] <- dim(contrast_3[contrast_3$log2FoldChange < 1,])[1]
 #names <- rownames(contrast_3[contrast_3$log2FoldChange < 1,])
 #write.table(names, paste0(contrast_results_dir, "/","5_M_W_vs_8_M_W", "/", "5_M_W_vs_8_M_W_down"), col.names = F, row.names = F, quote = F)
-# contrast 5_M_D_vs_5_M_W
+# contrast 5_M_W_vs_5_T_D
 # down
 #df3[7,1] <- "5M_W-D"
 #df3[7,2] <- "Down"
 #df3[7,3] <- dim(contrast_4[contrast_4$log2FoldChange < 1,])[1]
 #names <- rownames(contrast_4[contrast_4$log2FoldChange < 1,])
-#write.table(names, paste0(contrast_results_dir, "/","5_M_D_vs_5_M_W", "/", "5_M_D_vs_5_M_W_down"), col.names = F, row.names = F, quote = F)
+#write.table(names, paste0(contrast_results_dir, "/","5_M_W_vs_5_T_D", "/", "5_M_W_vs_5_T_D_down"), col.names = F, row.names = F, quote = F)
 # up
 #df3[8,1] <- "5M_W-D"
 #df3[8,2] <- "Up"
 #df3[8,3] <- dim(contrast_4[contrast_4$log2FoldChange > 1,])[1]
 #names <- rownames(contrast_4[contrast_4$log2FoldChange > 1,])
-#write.table(names, paste0(contrast_results_dir, "/","5_M_D_vs_5_M_W", "/", "5_M_D_vs_5_M_W_up"), col.names = F, row.names = F, quote = F)
+#write.table(names, paste0(contrast_results_dir, "/","5_M_W_vs_5_T_D", "/", "5_M_W_vs_5_T_D_up"), col.names = F, row.names = F, quote = F)
 # contrast 8_M_W_vs_8_M_W
 # down
 #df3[9,1] <- "8M_W-D"
 #df3[9,2] <- "Down"
 #df3[9,3] <- dim(contrast_5[contrast_5$log2FoldChange < 1,])[1]
 #names <- rownames(contrast_5[contrast_5$log2FoldChange < 1,])
-#write.table(names, paste0(contrast_results_dir, "/","8_M_D_vs_8_M_W", "/", "8_M_D_vs_8_M_W_down"), col.names = F, row.names = F, quote = F)
+#write.table(names, paste0(contrast_results_dir, "/","8_M_W_vs_8_T_D", "/", "8_M_W_vs_8_T_D_down"), col.names = F, row.names = F, quote = F)
 # up 
 #df3[10,1] <- "8M_W-D"
 #df3[10,2] <- "Up"
 #df3[10,3] <- dim(contrast_5[contrast_5$log2FoldChange > 1,])[1]
 #names <- rownames(contrast_5[contrast_5$log2FoldChange > 1,])
-#write.table(names, paste0(contrast_results_dir, "/","8_M_D_vs_8_M_W", "/", "8_M_D_vs_8_M_W_up"), col.names = F, row.names = F, quote = F)
+#write.table(names, paste0(contrast_results_dir, "/","8_M_W_vs_8_T_D", "/", "8_M_W_vs_8_T_D_up"), col.names = F, row.names = F, quote = F)
 
 ###
 #df3$Contrast <- factor(df3$Contrasts, levels = c("10 B0", "10 B", "10 M", "10 P", "270 B0", "270 B", "270 M", "270 P"))
@@ -114,7 +113,7 @@ library(svglite, lib= "/Storage/data1/jorge.munoz/NRGSC.new/libraries")
 #df3$Treatment <- c("D" "NR", "NR", "NR", "NR", "NR", "NR", "NR", "R", "R", "R", "R", "R", "R", "R", "R")
 #cols <- wes_palette("GrandBudapest1", 2, type = "continuous")
 cols <- c("#ECCBAE", "#D69C4E")
-df3$Contrast <- factor(df3$Contrast, levels = c("5_T_D_vs_5_M_D", "8_T_D_vs_8_M_D"))
+df3$Contrast <- factor(df3$Contrast, levels = c("5_M_W_vs_5_T_D", "8_M_W_vs_8_T_D"))
 DEG_PLOT <- ggplot(df3, aes(x=Contrast, y=DEGs, fill=Type, label = DEGs)) +
   geom_bar(stat="identity", position = "stack", size = 1)+
   geom_text(size = 6, position = position_stack(vjust = 0.5)) +	
@@ -140,4 +139,4 @@ DEG_PLOT <- ggplot(df3, aes(x=Contrast, y=DEGs, fill=Type, label = DEGs)) +
          plot.title = element_text(hjust = 0.5))
 
 
-ggsave(DEG_PLOT, filename = "../results/DEG_stackplot.png", device= "png" ,units = "cm",width = 45/1.5, height = 17,dpi = 320)
+ggsave(DEG_PLOT, filename = "../results/TDvsTW_DEG_stackplot.png", device= "png" ,units = "cm",width = 45/1.5, height = 17,dpi = 320)
